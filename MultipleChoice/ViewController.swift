5f9664a62d8f444cb27e68d6c96de65e8eba9cbf
Trent Stanley <tstanley@lcs.on.ca>
2019-12-05 14:43:24 -0500

    Initial Commit

---
+++ MultipleChoice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleChoice
+//
+//  Created by Stanley, Trent on 2019-12-05.
+//  Copyright © 2019 Stanley, Trent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
